f3ff109d7a1734bbf4514eb7db5bea6967ab27dc
RTgrave <stalker3067@gmail.com>
2020-09-20 23:05:24 +0300

    Add files via upload

---
+++ Praktika3/Praktika3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -12;
+            int b = 0; //кол-во чисел
+            int result = 0;
+            for (int i = a; i <= -65; i++)
+            {
+                a = a + i;
+                b++;
+            }
+            result = a / b;
+            Console.WriteLine(result);
+        }
+    }
+}
